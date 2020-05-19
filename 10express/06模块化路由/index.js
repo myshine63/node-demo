2e35ad70192834af37a44e208a5b099543c03315
myshine63 <1685738560@qq.com>
2020-05-19 20:37:45 +0800

    静态资源处理

--- 10express/06模块化路由/index.js
+++ 10express/06模块化路由/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const user = require('./router/user');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/user', user);
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(3000, () => {
   console.log('服务已经启动')
 });
