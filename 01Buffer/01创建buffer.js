d84d4e0095e615f75432b56b39a8f0f4ab3126b5
myshine63 <1685738560@qq.com>
2020-05-25 16:22:38 +0800

    修改了buffer中部分内容

--- 01Buffer/01创建buffer.js
+++ 01Buffer/01创建buffer.js
@@ -1,39 +1,39 @@
 // alloc创建的buff永远是堆外分配内存，默认用0去填充
 function alloc() {
-    let a = Buffer.alloc(5); // 默认用0填充
-    let b = Buffer.alloc(5, 100); // 转换成16进制
-    let c = Buffer.alloc(5, 256); // 先取余256 然后在转换成16进制
-    let d = Buffer.alloc(5, "abc");// 默认编码是utf8
-    let e = Buffer.alloc(5, "abc", "base64");
-    let flag = Buffer.isBuffer(a);
-    console.log(a);
-    console.log(b);
-    console.log(c);
-    console.log(d);
-    console.log(e);
-    console.log(flag);
+  let a = Buffer.alloc(5); // 默认用0填充
+  let b = Buffer.alloc(5, 100); // 转换成16进制
+  let c = Buffer.alloc(5, 256); // 先取余256 然后在转换成16进制
+  let d = Buffer.alloc(5, "abc");// 默认编码是utf8
+  let e = Buffer.alloc(5, "abc", "base64");
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
+  console.log(e);
+  console.log(Buffer.isBuffer(a));
 }
 
 // alloc();
+
 // allocUnsafe创建的buff的size小于poolSize/2时会使用默认的buff内存池分配内存。
-// 而且分配时没有进行初始化，可能保护旧数据
+// 而且分配时没有进行初始化，可能包含旧数据
 function allocUnsafe() {
-    let a = Buffer.allocUnsafe(10);
-    let b = Buffer.allocUnsafe(10);
-    b.fill(100);
-    console.log(a);
-    console.log(b);
+  let a = Buffer.allocUnsafe(10);
+  let b = Buffer.allocUnsafe(10);
+  b.fill(100);
+  console.log(a);
+  console.log(b);
 }
 
 // allocUnsafe();
 
 function from() {
-    let a = Buffer.from([1, 2, 3]); // 数组的每个元素需要为数字
-    let b = Buffer.from("abc",0,20); // 默认utf8编码
-    let c = Buffer.from(b); // 复制一个buffer
-    console.log(a);
-    console.log(b);
-    console.log(c);
+  let a = Buffer.from([1, 2, 1024]); // 数组的每个元素需要为数字
+  let b = Buffer.from(a); // 复制一个buffer
+  let c = Buffer.from('abc', 'utf-8');
+  console.log(a);
+  console.log(b);
+  console.log(c);
 }
 
 from();
--- 01Buffer/02Buffer类方法.js
+++ 01Buffer/02Buffer类方法.js
@@ -3,24 +3,28 @@
 // UTF8编码一个英文字母和符号1字节，汉字3字节，汉字符号3字节
 // UNICODE编码一个英文2个字节，一个汉字2个字节，因为标点和中文标点2字节
 function bufferSize() {
-    let a = Buffer.alloc(10);
-    let b = Buffer.from("中国","ascii");
-    let c = Buffer.from("中国","utf8");
-    let d = Buffer.from("abc");
-    let e = Buffer.from("abc","base64");
-    console.log(Buffer.byteLength(a)); //10
-    console.log(Buffer.byteLength(b)); //2
-    console.log(Buffer.byteLength(c)); //6
-    console.log(Buffer.byteLength(d)); //3
-    console.log(Buffer.byteLength(e)); //2
+  let a = Buffer.alloc(10);
+  let b = Buffer.from("中国", "ascii");
+  let c = Buffer.from("中国", "utf8");
+  let d = Buffer.from("abc");
+  let e = Buffer.from("abc", "base64");
+  console.log(Buffer.byteLength(a)); //10
+  console.log(Buffer.byteLength(b)); //2
+  console.log(Buffer.byteLength(c)); //6
+  console.log(Buffer.byteLength(d)); //3
+  console.log(Buffer.byteLength(e)); //2
+  console.log(Buffer.byteLength('abc', 'utf-8')); //3
 }
-bufferSize();
+
+// bufferSize();
 
 function concat() {
-    let a = Buffer.alloc(5,10);
-    let b = Buffer.alloc(5,12);
-    let c = Buffer.concat([a,b],10); // 添加size可以加快运行速度。
-    console.log(c);
+  let a = Buffer.alloc(5, 10);
+  let b = Buffer.alloc(5, 12);
+  let c = Buffer.concat([a, b], 10); // 添加size可以加快运行速度。
+  console.log(c);
 }
-concat();
 
+// concat();
+
+console.log(Buffer.poolSize);// buffer 字节默认分配的缓冲池大小
